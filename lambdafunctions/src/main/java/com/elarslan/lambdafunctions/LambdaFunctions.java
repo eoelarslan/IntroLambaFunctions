96941fc5526225f82e56c863a9c036d0db3f84aa
eoelarslan <ersin.elarslan@gmail.com>
2019-12-07 17:53:47 +0300

    Initial commit

---
+++ lambdafunctions/src/main/java/com/elarslan/lambdafunctions/LambdaFunctions.java
@@ -0,0 +1,11 @@
+package com.elarslan.lambdafunctions;
+
+import java.io.IOException;
+
+
+public class LambdaFunctions {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello Lambda");
+    }
+}
